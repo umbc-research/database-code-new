f44c008071bedd6cdce8369251dc6545318b208b
navyassassin <mb17@umbc.edu>
2024-10-15 11:56:12 -0700

    added map_QueryList

--- query_database.py
+++ query_database.py
@@ -1,9 +1,21 @@
 #!/usr/home/mb17/database_work/backend/bin/python3
 
-#import argparse
+#######################################
+## Name: query_database.py 
+## Purpose: To provide a reasonably easy
+## program to run to query observatory data
+## Author: Max Breitmeyer
+## Date: 20241015
+## Arguments: N/A
+#######################################
+
+
 import get_password
 import read_files
 import pymysql.cursors
+
+
+## Global variables, needed to connect to database. 
 
 DATABASE = "obsstor_test"
 TABLE = "fits_metadata"
@@ -12,6 +24,12 @@ HOST = "localhost"
 USER = "root"
 CHARSET = "utf8mb4"
 
+########################################################
+# Name: connect
+# Purpose: To allow for connection to the DB
+# Arguments: N/A
+# Return: connection (cursor.DictCursor)
+########################################################
 
 def connect():
     connection = pymysql.connect(host=HOST,
@@ -21,6 +39,12 @@ def connect():
                                  charset=CHARSET,
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
+########################################################
+# Name: get_fields
+# Purpose: Get all fields that are queriable from the DB Table
+# Arguments: connection (DictCursor);
+# Return: queryBy (list)
+########################################################
 
 def get_fields(connection):
     queryBy = []
@@ -34,23 +58,44 @@ def get_fields(connection):
                 queryBy.append(indexDict["Field"])
             return queryBy
 
-def welcome_message(queryList):
-    fieldLength = len(queryList)
-    print("Welcome to UMBC's Observatory Data Quierier.\n")
-    print("Enter the number associated with the field you would like to query")
-    fieldNumber = 0
-    while fieldNumber < fieldLength:
-        print(str(fieldNumber) + ": " + queryList[fieldNumber])
-        fieldNumber += 1
-    queryField = input()
-    return queryField
+########################################################
+# Name: map_QueryList
+# Purpose: helper function to map list of querieable items to numbers
+# Arguments: queryList (List);
+# Return: fieldDict (dictionary)
+########################################################
 
+def map_QueryList(queryList):
+    fieldNumber = 0
+    fieldLength = len(queryList)
+    fieldDict = {}
+    while fieldNumber < fieldLength:
+        fieldDict[fieldNumber] = queryList[fieldNumber]
+        fieldNumber += 1
+    return fieldDict
+
+########################################################
+# Function: welcome_message
+# Purpose: Gives a welcome message to users, and displays options to query the table
+# Arguments: fieldDict (List); queriable fields from DB
+# Return: queried (List); list of fields the user want to query
+########################################################
+
+def welcome_message(queryDict):
+    print("Welcome to UMBC's Observatory Data Quierier.\n")
+    print("Enter the number associated with the field you would like to query (if you would like to query more than one field enter multiple numbers separated by commas):")
+    for key in queryDict:
+        print(str(key) + ": " + queryDict[key])
+    queryFieldsInput = input()
+    queried = queryFieldsInput.split(",")
+    return queried
 
 
 def main():
     connection = connect()
     queryList = get_fields(connection)
-    welcome_message(queryList)
+    fieldDict = map_QueryList(queryList)
+    queried = welcome_message(fieldDict)
 
 if __name__ == "__main__":
     main()
